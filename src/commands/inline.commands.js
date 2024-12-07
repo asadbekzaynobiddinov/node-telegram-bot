a505129486a0a16ae3a0508ee58f7848d97f26d7
Asadbek Zaynobiddinov <asadbekzaynobiddinov51@gmail.com>
2024-12-07 18:21:59 +0500

    Buglar tuzatildi

--- src/commands/inline.commands.js
+++ src/commands/inline.commands.js
@@ -59,6 +59,7 @@ export const callBackFunction = async (ctx) => {
             case 'no':
                 const paymentNo = await Payment.findById(id)
                 await ctx.api.sendMessage(paymentNo.user_id, "Hisob to'ldirish haqidagi arizangiz bekor qilindi 😔\nQaytadan urinib ko'ring")
+                await ctx.editMessageReplyMarkup()
                 break;
 
             case 'confirm':
